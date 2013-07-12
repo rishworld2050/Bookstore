d81d48a8ddd47d61b9c10b41aaee8d685950a679
Rishab Kumar Agarwal <rishworld2050@gmail.com>
2013-07-12 13:07:08 +0530

    final build

---
+++ Project Samples/opencart/upload/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\opencart\upload/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\opencart\upload/system/');
+define('DIR_DATABASE', 'C:\wamp\www\opencart\upload/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\opencart\upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\opencart\upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\opencart\upload/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\opencart\upload/image/');
+define('DIR_CACHE', 'C:\wamp\www\opencart\upload/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\opencart\upload/download/');
+define('DIR_LOGS', 'C:\wamp\www\opencart\upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'blog');
+define('DB_PREFIX', 'oc_');
+?>
